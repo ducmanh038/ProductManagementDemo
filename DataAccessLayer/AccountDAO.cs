a3681e645a8181dcee24b0ff7b9cdc8566bfc2ec
ducmanh038 <leducmanh038@gmail.com>
2024-06-11 22:37:15 +0700

    Add files via upload
    ProductManagementDemo

---
+++ DataAccessLayer/AccountDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+namespace DataAccessLayer
+{
+    public class AccountDAO
+    {
+        public static AccountMember GetAccountById(string accountId)
+        {
+            AccountMember accountMember = new AccountMember();
+            if (accountId.Equals("PS0001"))
+            {
+                accountMember.MemberId = accountId;
+                accountMember.MemberPassword = "@1";
+                accountMember.MemberRole = 1;
+            }
+            return accountMember;
+        }
+    }
+}
---
+++ Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using Repositories;
+using DataAccessLayer;
+namespace Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryService iCategoryRepository;
+
+        public CategoryService()
+        {
+            iCategoryRepository = new CategoryRepository();
+        }
+        public List<Category> GetCategories()
+        {
+            return iCategoryRepository.GetCategories();
+        }
+    }
+}
